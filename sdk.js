7d196d648b949732656f859b65148bd421db01f7
DumDumGenius <dumdumgenius@gmail.com>
2019-01-19 02:06:02 -0800

    Create sdk.js

---
+++ sdk.js
@@ -0,0 +1,23 @@
+      var targetDate = new moment('2019-01-19', 'YYYY-MM-DD hh:mm');
+      var countdownInterval = setInterval(function () {
+        var countElem = document.getElementById('count');
+        secondsDiff = targetDate.diff(new moment(), 'seconds');
+        if (secondsDiff > 0) {
+          countElem.innerHTML = targetDate.diff(new moment(), 'seconds');
+        } else {
+          document.getElementById('count-down-text').innerHTML = 'Messi Yang will buy a 
+
+switch before Zoe Shen comes to Sydney, te amo mi novia y feliz cumpleanos!<br/>Messi <3 
+
+Zoe Shen';
+          document.getElementById('count-down-circle').style.display = 'none';
+          var switchImageElem = document.getElementById('switch-image');
+          switchImageElem.style.display = 'block';
+          switchImageElem.src = 'https://encrypted-tbn1.gstatic.com/shopping?
+
+q=tbn:ANd9GcSr8urXBqWXY3OK3UI_xMJqB6uyOjIUHLyo7kiUUjWGjXXR1tIyEB_4T17wdpVnvINdT8Ro4na2eyU8p
+
+Pvq8ZLQBYp3ndZq5cLAEl2ARKK_GLI8LhiGHrGjGQ&usqp=CAE';
+          clearInterval(countdownInterval);
+        }
+      }, 1000);
